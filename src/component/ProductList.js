5ebf1a9dd170d7b93d6fb98a07403043351ad5b7
Ngo Tien Dat <ngotiendat345@icloud.com>
2021-04-09 20:42:39 +0700

    first commit

---
+++ src/component/ProductList.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Product from "./Product";
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <section id="smartphone" className="container-fluid pt-5 pb-5 bg-dark">
+          <h1 className="text-white text-center">BEST SMARTPHONE</h1>
+          <div className="row">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
